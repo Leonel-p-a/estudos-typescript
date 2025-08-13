493f935a54e9316ef59eb601d92384fd1a94b74c
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-08-13 17:38:59 -0300

    Update: Prosseguindo com os estudos de TypeScript

--- src/modules/generics/generic-promises-then.ts
+++ src/modules/generics/generic-promises-then.ts
@@ -1,24 +1,81 @@
 export const bootstrap = (): void => {
-    const promise = new Promise((resolve, reject) => {
-        if (false) {
-            reject('Rejeitada');
-        }
-        
-        resolve('Resolvida');
-    })
-
-    // Função assíncrona
-    async function promiseFunction() {
-        return 200;
+    interface Game {
+        id: number;
+        title: String;
+        genre: string;
+        year: number;
     }
 
-    promiseFunction();
-    
+    function isGameArray(data: any): data is Game[] {
+        return (
+            Array.isArray(data) &&
+            data.every(
+                (item) =>
+                    typeof item.id === 'number' &&
+                    typeof item.title === 'string' &&
+                    typeof item.year === 'number' &&
+                    typeof item.genre === 'string'
+            )
+        )
+    }
+
     fetch('https://argus-academy.com/mock/api/games/')
         .then(response => {
             if (!response.ok) {
                 console.error('Erro HTTP: ', `${response.status} - ${response.statusText}`);
             }
-            response.json().then(data => console.log(data))     
+
+            // // Type Assertion
+            // return response.json() as Promise<Array<Game>>;  
+            return response.json();
+        }).then(data => {
+            if (isGameArray(data)) {
+                console.log(data);
+            } else {
+                console.error('Tipo de dado inesperado');
+            }
+        })
+
+        // --------
+
+        interface Car {
+        id: number;
+        brand: String;
+        model: string;
+        year: number;
+        type: string;
+        engine: string;
+    }
+
+    function isCarArray(data: any): data is Array<Car> {
+        return (
+            Array.isArray(data) &&
+            data.every(
+                (item) =>
+                    typeof item.id === 'number' &&
+                    typeof item.brand === 'string' &&
+                    typeof item.model === 'string' &&
+                    typeof item.year === 'number' &&
+                    typeof item.type === 'string' &&
+                    typeof item.engine === 'string'
+            )
+        )
+    }
+
+    fetch('https://argus-academy.com/mock/api/cars/')
+        .then(response => {
+            if (!response.ok) {
+                console.error('Erro HTTP: ', `${response.status} - ${response.statusText}`);
+            }
+
+            // // Type Assertion
+            // return response.json() as Promise<Array<Game>>;  
+            return response.json();
+        }).then(data => {
+            if (isCarArray(data)) {
+                console.log(data);
+            } else {
+                console.error('Tipo de dado inesperado');
+            }
         })
 };
